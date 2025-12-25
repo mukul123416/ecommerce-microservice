447eb9155f4a6cd641943665cd02f85b07398c4c
mukul sharma <Mukul Sharma>
2025-12-25 15:08:08 +0530

    Skip all test cases

--- ApiGatewayService/src/test/java/com/ec/apigateway/service/ApiGatewayService/ApiGatewayServiceApplicationTests.java
+++ ApiGatewayService/src/test/java/com/ec/apigateway/service/ApiGatewayService/ApiGatewayServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package com.ec.apigateway.service.ApiGatewayService;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class ApiGatewayServiceApplicationTests {
 
 	@Test
--- ServiceRegistry/src/test/java/com/ec/service/registry/ServiceRegistryApplicationTests.java
+++ ServiceRegistry/src/test/java/com/ec/service/registry/ServiceRegistryApplicationTests.java
@@ -1,9 +1,11 @@
 package com.ec.service.registry;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class ServiceRegistryApplicationTests {
 
 	@Test
