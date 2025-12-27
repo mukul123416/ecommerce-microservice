607961d2cb292ca000df6590ff86f37390733f91
mukul sharma <Mukul Sharma>
2025-12-27 14:37:16 +0530

    Add h2 in memory databse config

--- NotificationService/src/test/java/com/ec/notification/service/NotificationServiceApplicationTests.java
+++ NotificationService/src/test/java/com/ec/notification/service/NotificationServiceApplicationTests.java
@@ -2,7 +2,13 @@ package com.ec.notification.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.datasource.url=jdbc:h2:mem:testdb",
+		"spring.datasource.driverClassName=org.h2.Driver",
+		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+		"spring.cloud.config.enabled=false",
+		"spring.kafka.bootstrap-servers=localhost:9092"
+})
 class NotificationServiceApplicationTests {
 
 	@Test
